943b31b60fbcc9d6a0238e957a7dc8a0be6eebc5
Tyler <auslitztyler@gmail.com>
2021-01-14 15:16:46 -0500

    Set up some structure

--- RTSFreeGlut/Main.cpp
+++ RTSFreeGlut/Main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include "GL/glut.h"
 
+void idleFunc() {
+
+	glutPostRedisplay();
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSwapBuffers();
+}
+
+void windowReshape(int x, int y) {
+	//printf("x: %i, y: %i\n", x, y);
+	glViewport(0, 0, x, y);
+}
+
 int main(int argv, char **args) {
+	glutInit(&argv, args);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("RTS Test");
+	glutIdleFunc(idleFunc);
+	glutDisplayFunc(display);
+	glutReshapeFunc(windowReshape);
+	glutFullScreen();
+	glutMainLoop();
 	return 0;
 }
---
+++ RTSFreeGlut/unit.h
@@ -0,0 +1,16 @@
+#pragma once
+class unit {
+public:
+	unit() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	~unit() {
+
+	}
+
+private:
+	double x, y, z;
+
+};
