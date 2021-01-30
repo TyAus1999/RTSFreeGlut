43cb6c7a8a5530d9bc68e97c8ba3c0a03b9b6d2c
Tyler <auslitztyler@gmail.com>
2021-01-29 22:54:39 -0500

    Working time based movement system

---
+++ RTSFreeGlut/Engine.h
@@ -0,0 +1,88 @@
+#pragma once
+#include "GL/freeglut.h"
+#include "GL/glut.h"
+#include <immintrin.h>
+
+//Want to add multithreading, mutexs is for a task in the future
+class Engine {
+public:
+	Engine(char *windowName, int argv, char **args) {
+		glutInit(&argv, args);
+		glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_MULTISAMPLE | GLUT_DEPTH);
+		glutCreateWindow(windowName);
+
+		glEnable(GL_CULL_FACE);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GLUT_MULTISAMPLE);
+
+		glCullFace(GL_FRONT);
+	}
+	void setSize(int w, int h) {
+		glutReshapeWindow(w, h);
+	}
+	void setIdle(void (*idleFunc)(void)) {
+		glutIdleFunc(idleFunc);
+	}
+	void setDisplay(void (*displayFunc)(void)) {
+		glutDisplayFunc(displayFunc);
+	}
+	void setReshape(void (*reshapeFunc)(int, int)) {
+		glutReshapeFunc(reshapeFunc);
+	}
+	void start() {
+		glutMainLoop();
+	}
+};
+
+class Camera {
+public:
+	double coords[4];
+	double lookingAt[4];
+	double up[4];
+	Camera(double x, double y, double z) {
+		coords[0] = x;
+		coords[1] = y;
+		coords[2] = z;
+
+		lookingAt[0] = x;
+		lookingAt[1] = y;
+		lookingAt[2] = z + 5.0;
+
+		up[0] = 0;
+		up[1] = 1;
+		up[2] = 0;
+
+		//Only useful for avx
+		coords[3] = 0;
+		lookingAt[3] = 0;
+		up[3] = 0;
+	}
+	void moveByVel(double x, double y, double z, double deltaT) {
+		double toAdd[4];
+		toAdd[0] = x;
+		toAdd[1] = y;
+		toAdd[2] = z;
+		toAdd[3] = 0;
+		__m256d _velocities = _mm256_load_pd(toAdd);
+		__m256d _deltaT = _mm256_broadcast_sd(&deltaT);
+		__m256d _addAmount = _mm256_mul_pd(_velocities, _deltaT);
+		this->moveToAdd(_addAmount);
+	}
+	void draw() {
+		gluLookAt(coords[0], coords[1], coords[2],
+			lookingAt[0], lookingAt[1], lookingAt[2],
+			up[0], up[1], up[2]);
+	}
+private:
+	void moveToAdd(__m256d _toAdd) {
+		__m256d _coords, _lookingAt, _resultCoords, _resultLookingAt;
+		_coords = _mm256_load_pd(coords);
+		_lookingAt = _mm256_load_pd(lookingAt);
+		_resultCoords = _mm256_add_pd(_toAdd, _coords);
+		_resultLookingAt = _mm256_add_pd(_toAdd, _lookingAt);
+		_mm256_store_pd(coords, _resultCoords);
+		_mm256_store_pd(lookingAt, _resultLookingAt);
+		coords[3] = 0;
+		lookingAt[3] = 0;
+	}
+};
--- RTSFreeGlut/Main.cpp
+++ RTSFreeGlut/Main.cpp
@@ -2,25 +2,45 @@
 #include "GL/glut.h"
 #include "unit.h"
 #include <vector>
+#include "Engine.h"
+#include <chrono>
 using namespace std;
 typedef unsigned long long u64;
 
 
 vector<unit> units;
+Camera* currentCamera;
+double camVel = -10;
+u64 prevTime;
+
+u64 getCurrentTimeMS() {
+	u64 out = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+	return out;
+}
 
 void idleFunc() {
+	u64 currentTime = getCurrentTimeMS();
+	double deltaT=(double)currentTime-(double)prevTime;
+	deltaT /= 1000;//Convert to seconds
 	for (int i = 0; i < units.size(); i++) {
 		unit* u = &units[i];
-		u->rotateAdd(0.5, 0);
-		u->rotateAdd(0.5, 1);
+		//u->rotateAdd(0.5, 0);
+		//u->rotateAdd(0.5, 1);
 	}
+	currentCamera->moveByVel(0,0,camVel,deltaT);
+	if (currentCamera->coords[2] < -50 || currentCamera->coords[2] > -5) {
+		camVel *= -1;
+		currentCamera->moveByVel(0,0,camVel,deltaT);
+	}
+	prevTime = currentTime;
 	glutPostRedisplay();
 }
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	gluLookAt(5.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	currentCamera->draw();
+	//gluLookAt(5.0, 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	for (int i = 0; i < units.size(); i++) {
 		unit* u = &units[i];
 		//u->rotateAdd(1, 1);
@@ -45,7 +65,21 @@ int main(int argv, char **args) {
 	unit toAdd(0.0,0.0,0.0);
 	toAdd.move(0, 0, -3);
 	units.push_back(toAdd);
-	
+
+	Camera c0(0, 5, -10);
+	c0.lookingAt[1] = 0;
+
+	currentCamera = &c0;
+
+	char winName[] = "RTS Test\0";
+	Engine e(winName, argv, args);
+	e.setSize(500, 500);
+	e.setDisplay(display);
+	e.setIdle(idleFunc);
+	e.setReshape(windowReshape);
+	e.start();
+
+	/*
 	glutInit(&argv, args);
 	glutInitWindowSize(500, 500);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_MULTISAMPLE | GLUT_DEPTH);
@@ -62,6 +96,6 @@ int main(int argv, char **args) {
 	//glutKeyboardFunc(keyboardFunc);
 	//glutFullScreen();
 	glutMainLoop();
-	printf("Closing\n");
+	*/
 	return 0;
 }
